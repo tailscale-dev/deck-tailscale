9d806242ccb65bf46395b49e3435d16314492117
Duncan Gibson <legowerewolf@gmail.com>
2023-07-12 17:54:45 -0400

    Split install output into lines.

--- tailscale.sh
+++ tailscale.sh
@@ -16,18 +16,22 @@ pushd . > /dev/null
 dir="$(mktemp -d)"
 cd "${dir}"
 
-echo -n "Installing Tailscale: Getting version..."
+echo -n "Getting version..."
 
 # get info for the latest version of Tailscale
 tarball="$(curl -s 'https://pkgs.tailscale.com/stable/?mode=json' | jq -r .Tarballs.amd64)"
 version="$(echo ${tarball} | cut -d_ -f2)"
 
-echo -n "got ${version}. Downloading..."
+echo "got ${version}."
+
+echo -n "Downloading..."
 
 # download the Tailscale package itself
 curl -s "https://pkgs.tailscale.com/stable/${tarball}" -o tailscale.tgz
 
-echo -n "done. Installing..."
+echo "done."
+
+echo -n "Installing..."
 
 # extract the tailscale binaries
 tar xzf tailscale.tgz
@@ -53,6 +57,10 @@ cp -rf tailscale /var/lib/extensions/
 popd > /dev/null
 rm -rf "${dir}"
 
+echo "done."
+
+echo -n "Starting services..."
+
 systemctl enable systemd-sysext --now
 
 systemd-sysext refresh > /dev/null 2>&1
@@ -61,4 +69,6 @@ systemctl daemon-reload > /dev/null
 systemctl enable tailscaled --now
 
 echo "done."
+
+
 echo "If updating, reboot or run the following to finish the process: sudo systemctl restart tailscaled"
