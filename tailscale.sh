61b47d9522511b83eb612cb537c20e5ec96ae4d4
Zac Holland <zacharyholland@gmail.com>
2023-12-18 13:56:22 -0800

    fix source command

--- tailscale.sh
+++ tailscale.sh
@@ -116,7 +116,7 @@ if ! command -v tailscale &> /dev/null; then
   echo "Tailscale is installed and running but the binaries are not in your path yet."
   echo "Restart your session or run the following command to add them:"
   echo 
-  echo "source /etc/tailscale"
+  echo "source /etc/profile.d/tailscale.sh"
   echo
 fi
 
