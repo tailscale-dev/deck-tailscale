f582ec51e73adfc41074d0e8a70296c7e35a2031
Zac Holland <zacharyholland@gmail.com>
2023-12-18 13:00:46 -0800

    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+systemctl stop tailscaled
+systemctl disable tailscaled
+rm /etc/systemd/system/tailscaled.service
+rm /etc/default/tailscaled
+rm /etc/profile.d/tailscale.sh
+rm -rf /opt/tailscale/tailscale
