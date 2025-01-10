5b6c0544610f2cf4d9b553e0be381c58cfdbb6cb
sixcorners <sixcorners@gmail.com>
2025-01-10 16:02:43 -0500

    Remove override.conf in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,5 @@ rm /etc/systemd/system/tailscaled.service
 rm /etc/default/tailscaled
 rm /etc/profile.d/tailscale.sh
 rm -rf /opt/tailscale/tailscale
+rm /etc/systemd/system/tailscaled.service.d/override.conf
+rmdir /etc/systemd/system/tailscaled.service.d
