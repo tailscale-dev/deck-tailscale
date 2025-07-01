e19e61a1c81716138044fe0ea3d3170972ec427d
arrowmaster <arrow@arrowmaster.net>
2025-07-01 10:09:05 -0400

    Add atomic-update configurate to preserve files in /etc

--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,5 @@ systemctl disable tailscaled
 rm /etc/systemd/system/tailscaled.service
 rm /etc/default/tailscaled
 rm /etc/profile.d/tailscale.sh
+rm /etc/atomic-update.conf.d/tailscale.conf
 rm -rf /opt/tailscale/tailscale
